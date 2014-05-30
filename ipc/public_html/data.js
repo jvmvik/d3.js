25e73d15c83b342466767f786dccb8ab6d10c4c9
jvmvik <jvmvik@gmail.com>
2014-05-30 12:26:43 -0500

    Add IPC dependency graph (development)

---
+++ ipc/public_html/data.js
@@ -0,0 +1,38 @@
+var data = {"name": "A", "info": "tst", "children": [
+        {"name": "A1",
+            "type": "array",
+            "children": [
+                {"name": "A12"},
+                {"name": "A13"},
+                {"name": "A14"},
+                {"name": "A15"},
+                {"name": "A16"}
+            ]},
+        {"name": "A2",
+            "type": "array",
+            "children": [
+                {"name": "A21"},
+                {"name": "A22", "children": [
+                        {"name": "A221"},
+                        {"name": "A222"},
+                        {"name": "A223"},
+                        {"name": "A22"}
+                    ]},
+                {"name": "A23"},
+                {"name": "A24"},
+                {"name": "A25"}]},
+        {
+            "name": "A3",
+            "type": "array",
+            "children": [
+                {
+                    "name": "A31", 
+                    "type": "array",
+                    "children": [
+                        {"name": "A311"},
+                        {"name": "A312"},
+                        {"name": "A313"},
+                        {"name": "A314"},
+                        {"name": "A315"}
+                    ]}]}
+    ]};
